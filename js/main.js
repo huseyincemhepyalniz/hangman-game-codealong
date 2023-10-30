99237dd85de70793f2e6a30a5c62ed5420a5a5cc
John Doe <johndoe@example.com>
2023-10-30 15:30:29 +0100

    CSS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-console.log("js loaded....")
+
+//Question to appear
+
+let question = document.getElementById('question1');
+
+question.innerHTML = "It's a well-known dish in Europe, any ideas?";
+
+//European Dishes Words
+
+let europeanDishes = ["Paella", "Croissant", "Pizza", "Bratwurst", "Schnitzel", "Goulash", "Tiramisu", "Ratatouille"];
+
+//Random Word Selection
+let wordToGuess;
+function randomWordSelection() {
+    const randomIndex = Math.floor(Math.random() * europeanDishes.length);
+    let wordToGuess = europeanDishes[randomIndex];
+    console.log(wordToGuess);
+
+}
+
+
+//Game Variables
+
+let lettersGuessed = [];
+let attemptsRemaining = 7;
+
+
+function updateWordDisplay() {
+    const wordDisplay = document.getElementById("word-display");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
